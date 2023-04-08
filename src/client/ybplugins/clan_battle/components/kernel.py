15ea58da39c9a80fb3eb82506935e99e2af40eab
perfBleu <lunaticstarksm@gmail.com>
2023-04-08 15:10:27 +0800

    增加查树，优化帮助页

--- src/client/ybplugins/clan_battle/components/kernel.py
+++ src/client/ybplugins/clan_battle/components/kernel.py
@@ -391,6 +391,24 @@ def execute(self, match_num, ctx):
 		_logger.info('群聊 成功 {} {} {}'.format(user_id, group_id, cmd))
 		return "进度已重置\n当前档案编号已从 {} 切换为 {}".format(current_data_slot_record, available_empty_battle_id)
 
-
-
-
+	elif 21 <= match_num <= 26:
+		if len(cmd) != 2:
+			return
+		reply = ""
+		if match_num == 21:
+			_dic = self.query_tree(group_id=group_id, user_id=user_id)
+			for key in _dic:
+				if _dic[key] != []:
+					reply += f"挂在{key}的成员有：\n"
+					for item in _dic[key]:
+						reply += f"{self._get_nickname_by_qqid(int(item[0]))}:{item[1]}"
+		else:
+			_boss_num = match_num - 21
+			try:
+				_dic = self.query_tree(group_id=group_id, user_id=user_id, boss_id=_boss_num)
+			except KeyError:
+				return f"没有挂在{_boss_num}王的成员"
+			reply += f"挂在{_boss_num}的成员有：\n"
+			for item in _dic[str(_boss_num)]:
+				reply += f"{self._get_nickname_by_qqid(int(item[0]))}:{item[1]}"
+		return reply
